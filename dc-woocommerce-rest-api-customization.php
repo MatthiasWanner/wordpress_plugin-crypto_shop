eefc922f82b08b17b3721fcf5ccdf603088bc9c4
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-21 21:56:36 +0100

    :sparkles: (api) new custom key in data response

--- dc-woocommerce-rest-api-customization.php
+++ dc-woocommerce-rest-api-customization.php
@@ -6,3 +6,10 @@ Version: 1.0
 Author: Matthias Wanner
 Author URI: https://github.com/MatthiasWanner
 License: GPLv2 or later */ 
+
+add_filter('woocommerce_rest_prepare_product_object', 'get_custom_attributes', 10, 3);
+
+function get_custom_attributes($data, $product, $request) {
+    $data->data['custom'] = "test";
+    return $data;
+}
