010085ca4cccba6eabcb2ce621b643f34331e05f
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-21 21:51:52 +0100

    :tada: (init) Initial commit

---
+++ dc-woocommerce-rest-api-customization.php
@@ -0,0 +1,8 @@
+<?php /* 
+Plugin Name: Digital Copilote Woocommerce REST API Customization
+Plugin URI:  
+Description: Woocommerce REST API response customization
+Version: 1.0 
+Author: Matthias Wanner
+Author URI: https://github.com/MatthiasWanner
+License: GPLv2 or later */ 
