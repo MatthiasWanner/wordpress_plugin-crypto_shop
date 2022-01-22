5a5c391e408f6521c5bbe61c81f9625f2c71b1c7
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-23 00:07:47 +0100

    :building_construction: (terms) terms classes definition

---
+++ classes/class-cryptoshop-attribute-term.php
@@ -0,0 +1,16 @@
+<?php
+require('class-cryptoshop-term-metas.php');
+
+class CryptoShopAttributeTerm {
+    public int $id;
+    public string $name;
+    public string $slug;
+    public CryptoShopTermMetas $metas;
+
+    public function __construct($db_term_infos, string $parent_attribute_name) {
+        $this->id = $db_term_infos->term_id;
+        $this->name = $db_term_infos->name;
+        $this->slug = $db_term_infos->slug;
+        $this->metas = new CryptoShopTermMetas($this->id, $parent_attribute_name);
+    }
+}
---
+++ classes/class-term-db-infos.php
@@ -0,0 +1,7 @@
+<?php
+class TermDbInfos {
+    public int $term_id;
+    public string $name;
+    public string $slug;
+    public string $term_group; 
+}
