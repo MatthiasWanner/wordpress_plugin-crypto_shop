2b738bcbed89f975ae8b10321ede88a52f3a81b0
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-23 15:41:05 +0100

    :truck: (root) Rename root file

--- dc-cryptoshop.php
+++ dc-cryptoshop.php
@@ -1,6 +1,6 @@
 <?php /* 
 Plugin Name: Crypto Shop by DC
-Plugin URI:  
+Plugin URI: https://github.com/MatthiasWanner/wordpress_plugin-crypto_shop/
 Description: Crypto Shop WooCommerce customization by Digital Copilote
 Version: 1.0 
 Author: Matthias Wanner
