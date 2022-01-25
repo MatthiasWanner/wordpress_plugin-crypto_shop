ec8366c1e75478fc462c73b3cd7bb62597d03c27
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-25 15:18:31 +0100

    :construction: (begin) Add variation filter in plugin classe

--- classes/class-metashop-plugin.php
+++ classes/class-metashop-plugin.php
@@ -2,10 +2,11 @@
 class MetaShopPlugin {
     public function __construct() {
         require_once plugin_dir_path( __FILE__ ).'attribute/class-metashop-attribute.php';
-        add_filter('woocommerce_rest_prepare_product_object', array($this, 'get_custom_options'), 10, 3);
+        add_filter('woocommerce_rest_prepare_product_object', array($this, 'get_custom_product_options'), 10, 3);
+        add_filter('woocommerce_rest_prepare_product_variation_object', array($this, 'get_custom_variations_properties'), 10, 3);
     }
 
-    public function get_custom_options($response, $request){
+    public function get_custom_product_options($response, $request){
         // Customize response data
         $product_attributes = $response->data['attributes'];
     
@@ -16,6 +17,14 @@ class MetaShopPlugin {
             $response->data['custom_options'] = null;
         }
     
+        return $response;
+    }
+
+    public function get_custom_variations_properties($response, $request){
+        
+        $response->data['images'] = 'test';
+      
+        unset($response->data['image']);
         return $response;
     }
 }
