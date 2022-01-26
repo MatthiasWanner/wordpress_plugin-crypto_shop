217acbddeb4467f86a203bb8c004e801915ae7d1
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-26 15:06:48 +0100

    :sparkles: (product) auto generate short description if empty
    Based on product description, filtering table element html

--- classes/product/class-metashop-product.php
+++ classes/product/class-metashop-product.php
@@ -17,8 +17,12 @@ class MetaShopProduct {
 
     private function generate_short_description() {
         $product_description = $this->original_response['description'];
-        $short_description = "Tu n'as pas rempli de description courte pour ce produit 😞 ";
-        return $short_description;
+        $flags = PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY;
+        $html_parse_regex = '/(<[a-z0-9=\-:." ^\/]+\/>)|(<[^\/]+>[^<\/]+<\/[a-z0-9]+>)|(<[a-z0-9=\-:." ^\/]+>)/';
+        $exclude_parts_regex = '/<[\/]?(div|t([a-z]+))( ?(.)+]?)?>|(\n)|(\+\/-)/';
+        $parts = preg_split( $html_parse_regex, $product_description, -1, $flags);
+        $filtered_parts = array_values(array_filter($parts, fn($part) => !preg_match($exclude_parts_regex, $part)));
+        return implode(' ', $filtered_parts);
     }
 
 }
