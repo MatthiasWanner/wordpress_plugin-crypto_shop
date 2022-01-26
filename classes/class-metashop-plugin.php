46f02c89ec6defc85d162ce4db2fc2fcde7493a6
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-26 12:24:25 +0100

    :building_construction: (method) rename product filter hook callable

--- classes/class-metashop-plugin.php
+++ classes/class-metashop-plugin.php
@@ -4,11 +4,11 @@ class MetaShopPlugin {
         require_once plugin_dir_path( __FILE__ ).'attribute/class-metashop-attribute.php';
         require_once plugin_dir_path( __FILE__ ).'product/class-metashop-product.php';
         require_once plugin_dir_path( __FILE__ ).'variation/class-metashop-variation.php';
-        add_filter('woocommerce_rest_prepare_product_object', array($this, 'get_custom_product_options'), 10, 3);
+        add_filter('woocommerce_rest_prepare_product_object', array($this, 'get_custom_product_properties'), 10, 3);
         add_filter('woocommerce_rest_prepare_product_variation_object', array($this, 'get_custom_variations_properties'), 10, 3);
     }
 
-    public function get_custom_product_options($response, $request){
+    public function get_custom_product_properties($response, $request){
         // Customize response data
         $new_product = new MetaShopProduct($response->data);
         $product_attributes = $response->data['attributes'];
