8a8a5874f9e393c2705dda5a0c2a22d28c441743
MatthiasWanner <matthias.wanner@gmail.com>
2022-01-26 12:26:52 +0100

    :art: (product) prepare short description auto generate logic

--- classes/product/class-metashop-product.php
+++ classes/product/class-metashop-product.php
@@ -9,10 +9,16 @@ class MetaShopProduct {
 
     public function get_short_description() {
         if(!$this->original_response['short_description']) {
-            return "Il faut remplir la short_description";
+            return  $this->generate_short_description();
         } else {
             return $this->original_response['short_description'];
         }
+    }
+
+    private function generate_short_description() {
+        $product_description = $this->original_response['description'];
+        $short_description = "Tu n'as pas rempli de description courte pour ce produit 😞 ";
+        return $short_description;
     }
 
 }
